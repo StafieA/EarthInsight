0f24e5643e590e2ff0e5c5a4f3994ec97c8a2b6c
Andrei Stafie <andreistafie@gmail.com>
2024-11-23 14:57:46 +0200

    Linking between routes with NavLink

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Home</h1>
+      {/* <a href="/pricing">Pricing</a> */}
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Home;
